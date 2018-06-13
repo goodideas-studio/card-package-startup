566bc33ed5ec3169a5775de4f10ea6d7b98c0daf
SOJ <takayashi0825@gmail.com>
2018-06-13 15:32:31 +0800

    update CardVC in Team-B

--- Team-B/CardGameWithWeb/CardGameWithWeb/CardViewController.swift
+++ Team-B/CardGameWithWeb/CardGameWithWeb/CardViewController.swift
@@ -10,13 +10,20 @@ class JavaScriptFunc : NSObject, JavaScriptFuncProtocol {
     func saveData(_ character: String, background: String, conis coins: String) {
         UserDefaults.standard.setValue(coins, forKey: "coins")
         
-        let drawInfo = ["charactor": character, "background": "cardBackground0\(Int(background)!+1)"]
+        let drawInfo = ["character": character, "background": "cardBackground0\(Int(background)!+1)"]
         print(drawInfo)
         var cardLists : [[String:String]] = UserDefaults.standard.array(forKey: "record") as? [[String : String]] == nil ? [] : UserDefaults.standard.array(forKey: "record") as! [[String : String]]
         
         cardLists.append(drawInfo)
         UserDefaults.standard.setValue(cardLists, forKey: "record")
     }
+    
+    func returnCoins() -> String {
+        
+        return UserDefaults.standard.string(forKey: "coins")!
+        
+    }
+    
 }
 
 class CardViewController: UIViewController, UIWebViewDelegate {
@@ -31,19 +38,15 @@ class CardViewController: UIViewController, UIWebViewDelegate {
         mainWebView.loadRequest(request)
         
         let jsContext = mainWebView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as? JSContext
-        jsContext?.setObject(JavaScriptFunc(), forKeyedSubscript: "javaScriptCallToSwift" as (NSCopying & NSObjectProtocol)!)
+        jsContext?.setObject(JavaScriptFunc(), forKeyedSubscript: "javaScriptCallToSwift" as (NSCopying & NSObjectProtocol)?)
 
-        
-        
-        let context: JSContext = JSContext()
-        
-        context.evaluateScript("var num1 = 10; var num2 = 20;")
-        
-        let result: JSValue = context.evaluateScript("add(num1, num2)")
-        print("result: \(result)") // 30
+//        let context: JSContext = JSContext()
 
+        let testContext = self.mainWebView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as? JSContext
+        
        let fun = JavaScriptFunc()
         fun.saveData("0", background: "2", conis: "5000")
+        print(fun.returnCoins())
         
     }
 }
