8a783b1aa42c7d7d9b37b76c7e34ebbe060d266b
andyka1714 <andyka1714@gmail.com>
2018-06-13 14:42:15 +0800

    test money

--- Team-A/web/js/index.js
+++ Team-A/web/js/index.js
@@ -3,59 +3,93 @@ window.onload = function () {
   let cost = 20
   let monster
   let rollList = []
+  let status = 'hidden'
   let cardCover = document.querySelector('.card-cover')
   let cardEffect = document.querySelector('.card-effect')
 
-  for (let i = 0; i < 20; i++) {
-    let rand = Math.floor((Math.random() * cardList.length))
-    rollList.push(cardList[rand])
-  }
-
   let rollListDOM = document.querySelector('.roll-list')
-  for (let i = 0; i < rollList.length; i++) {
-    let newItem = document.createElement('li')
-    switch (rollList[i].type) {
-      case 0: newItem.classList.add('white-card'); break
-      case 1: newItem.classList.add('green-card'); break
-      case 2: newItem.classList.add('blue-card'); break
-      case 3: newItem.classList.add('red-card'); break
-    }
-    newImg = document.createElement('img')
-    newImg.src = `${rollList[i].src}`
-    newItem.append(newImg)
-    rollListDOM.append(newItem)
-  }
 
   let theBalance = document.querySelector('.balance')
+
+  var test = javaScriptCallToSwift.test('test')
+  theBalance.innerHTML = test
+
+  function randomCard () {
+    for (let i = 0; i < 20; i++) {
+      let rand = Math.floor((Math.random() * cardList.length))
+      rollList.push(cardList[rand])
+    }
+
+    for (let i = 0; i < rollList.length; i++) {
+      let newItem = document.createElement('li')
+      switch (rollList[i].type) {
+        case 0: newItem.classList.add('white-card'); break
+        case 1: newItem.classList.add('green-card'); break
+        case 2: newItem.classList.add('blue-card'); break
+        case 3: newItem.classList.add('red-card'); break
+      }
+      newImg = document.createElement('img')
+      newImg.src = `${rollList[i].src}`
+      newItem.append(newImg)
+      rollListDOM.append(newItem)
+    }
+  }
 
   let rollBtn = document.querySelector('.roll-btn')
   let testBtn = document.querySelector('.test-btn')
   rollBtn.addEventListener('click', e => {
+    console.log()
+
     if (balance - cost < 0) {
       alert('餘額不足請盡快儲值！！')
       return
     }
-    balance -= cost
-    theBalance.innerHTML = balance
-    monster = rollList[rollList.length - 1]
-    console.log(rollList, monster)
-    removeGlow(cardEffect)
-    turnOver(cardCover)
-    roll(rollListDOM)
+
+    if (status === 'show') {
+      rollListDOM.innerHTML = ''
+      rollList = []
+      addGlow(cardEffect)
+      hideCard(cardCover)
+      resetRoll(rollListDOM)
+      status = 'hidden'
+    } else {
+      randomCard()
+      removeGlow(cardEffect)
+      showCard(cardCover)
+      doRoll(rollListDOM)
+      status = 'show'
+      balance -= cost
+      theBalance.innerHTML = balance
+      monster = rollList[rollList.length - 1]
+      console.log(rollList, monster)
+    }
   })
-  testBtn.addEventListener('click', javaScriptCallToSwift.test('test'))
+
+  testBtn.addEventListener('click', javaScriptCallToSwift.test(balance))
 }
 
 function removeGlow (element) {
   element.classList.remove('glow')
 }
 
-function turnOver (element) {
+function addGlow (element) {
+  element.classList.add('glow')
+}
+
+function showCard (element) {
   element.classList.add('disable')
 }
 
-function roll (element) {
+function hideCard (element) {
+  element.classList.remove('disable')
+}
+
+function doRoll (element) {
   element.classList.add('rolled')
+}
+
+function resetRoll (element) {
+  element.classList.remove('rolled')
 }
 
 let cardList = [
