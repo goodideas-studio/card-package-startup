091f735f6f0ab894328f6e9c2a67c242ac2955c6
SOJ <takayashi0825@gmail.com>
2018-06-13 17:17:07 +0800

    update in Team-B

--- Team-B/CardGameWithWeb/CardGameWithWeb/CardViewController.swift
+++ Team-B/CardGameWithWeb/CardGameWithWeb/CardViewController.swift
@@ -2,25 +2,34 @@ import UIKit
 import JavaScriptCore
 
 @objc protocol JavaScriptFuncProtocol: JSExport {
-    func saveData(_ character: String, background: String, conis: String)
-//    func test2(_ value: String, _ num: Int)
+    func saveData(_ character: Int, _ background: Int, _ conis: Int)
+//    func saveData(_ character: String, _ background: String)
 }
 
 class JavaScriptFunc : NSObject, JavaScriptFuncProtocol {
-    func saveData(_ character: String, background: String, conis coins: String) {
-        UserDefaults.standard.setValue(coins, forKey: "coins")
+    
+//    func saveData(_ character: String, _ background: String) {
+//        print("OK")
+//    }
+    
+    func saveData(_ character: Int, _ background: Int, _ coins: Int) {
+        print("OK")
         
-        let drawInfo = ["character": character, "background": "cardBackground0\(Int(background)!+1)"]
-        print(drawInfo)
+        
+        
+        UserDefaults.standard.setValue(String(coins), forKey: "coins")
+        
+        let drawInfo = ["character": String(character), "background": "cardBackground0\(background+1)"]
+//        print(drawInfo)
         var cardLists : [[String:String]] = UserDefaults.standard.array(forKey: "record") as? [[String : String]] == nil ? [] : UserDefaults.standard.array(forKey: "record") as! [[String : String]]
         
         cardLists.append(drawInfo)
         UserDefaults.standard.setValue(cardLists, forKey: "record")
     }
     
-    func returnCoins() -> String {
+    func returnCoins() -> Int {
         
-        return UserDefaults.standard.string(forKey: "coins")!
+        return UserDefaults.standard.integer(forKey: "coins")
         
     }
     
@@ -37,15 +46,12 @@ class CardViewController: UIViewController, UIWebViewDelegate {
         let request = URLRequest(url: url!)
         mainWebView.loadRequest(request)
         
-        let jsContext = mainWebView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as? JSContext
-        jsContext?.setObject(JavaScriptFunc(), forKeyedSubscript: "javaScriptCallToSwift" as (NSCopying & NSObjectProtocol)?)
-
-//        let context: JSContext = JSContext()
-
-        let testContext = self.mainWebView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as? JSContext
+                let jsContext = self.mainWebView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as? JSContext
+                jsContext?.setObject(JavaScriptFunc(), forKeyedSubscript: "javaScriptCallToSwift" as (NSCopying & NSObjectProtocol)!)
+        
         
        let fun = JavaScriptFunc()
-        fun.saveData("0", background: "2", conis: "5000")
+//        fun.saveData("0", background: "2", conis: "5000")
         print(fun.returnCoins())
         
     }
