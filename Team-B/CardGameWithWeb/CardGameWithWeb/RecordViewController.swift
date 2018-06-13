10826199e9e0367237bcf4f2cadd9614034dc344
Morris Lu <morris.lwt@gmail.com>
2018-06-13 13:57:01 +0800

    edit RecordViewController in Team-B

--- Team-B/CardGameWithWeb/CardGameWithWeb/RecordViewController.swift
+++ Team-B/CardGameWithWeb/CardGameWithWeb/RecordViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class RecordViewController: UIViewController {
     
+    @IBOutlet weak var recordCollectionView: UICollectionView!
     let charactorArray = ["6","10","20","24","27","42","46","56","69","90"]
     var playRecords = [[String:String]]()
     
@@ -32,13 +33,40 @@ class RecordViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
+        recordCollectionView.backgroundColor = .clear
+        
+        let firstRecord = [["charactor":"\(charactorArray[0])","background":"cardBackground05"],
+                           ["charactor":"\(charactorArray[1])","background":"cardBackground05"],
+                           ["charactor":"\(charactorArray[2])","background":"cardBackground04"],
+                           ["charactor":"\(charactorArray[3])","background":"cardBackground03"],
+                           ["charactor":"\(charactorArray[4])","background":"cardBackground02"],
+                           ["charactor":"\(charactorArray[5])","background":"cardBackground01"],
+                           ["charactor":"\(charactorArray[0])","background":"cardBackground05"],
+                           ["charactor":"\(charactorArray[1])","background":"cardBackground05"],
+                           ["charactor":"\(charactorArray[2])","background":"cardBackground04"],
+                           ["charactor":"\(charactorArray[3])","background":"cardBackground03"],
+                           ["charactor":"\(charactorArray[4])","background":"cardBackground02"],
+                           ["charactor":"\(charactorArray[5])","background":"cardBackground01"]]
+        UserDefaults.standard.set(firstRecord, forKey: "record")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let getPlayRecord = UserDefaults.standard.array(forKey: "record") else { return }
+        guard getPlayRecord.count > 0 else { return }
+        playRecords = getPlayRecord as! [[String : String]]
+        recordCollectionView.reloadData()
     }
     
 }
 extension RecordViewController:UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        if playRecords.count == 0 {
+            return 1
+        }else{
+            return playRecords.count
+        }
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let redAnimation = createImages(name: "card_aura_red_128x128_")
@@ -46,37 +74,28 @@ extension RecordViewController:UICollectionViewDataSource,UICollectionViewDelega
         let greenAnimation = createImages(name: "card_aura_green_128x128_")
         let grayAnimation = createImages(name: "card_aura_gray_128x128_")
         
-        let firstRecord = [["charactor":"\(charactorArray[0])","background":"cardBackground05"],
-                           ["charactor":"6","\(charactorArray[1])":"cardBackground05"],
-                           ["charactor":"6","\(charactorArray[2])":"cardBackground04"],
-                           ["charactor":"6","\(charactorArray[3])":"cardBackground03"],
-                           ["charactor":"6","\(charactorArray[4])":"cardBackground02"],
-                           ["charactor":"6","\(charactorArray[5])":"cardBackground01"]]
-        playRecords.append(firstRecord)
+
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CardRecordCell
         
-        cell.recordLabel.text = "第 \(indexPath.item) 抽"
-        switch indexPath.item {
-        case 0:
-            cell.animationView.image = UIImage.animatedImage(with: redAnimation, duration: 2)
-            cell.colorView.image = UIImage(named: "cardBackground05")
-        case 1:
+        if playRecords.count == 0 {
+            cell.recordLabel.text = "還沒抽卡哦！"
+            return cell
+        }
+        
+        guard playRecords.count > 0 else { return cell }
+        cell.recordLabel.text = "第 \(indexPath.item + 1) 抽"
+        cell.colorView.image = UIImage(named: playRecords[indexPath.item]["background"]!)
+        cell.cardView.image = UIImage(named: playRecords[indexPath.item]["charactor"]!)
+        
+        switch playRecords[indexPath.item]["background"] {
+        case "cardBackground05":
+             cell.animationView.image = UIImage.animatedImage(with: redAnimation, duration: 2)
+        case "cardBackground04":
             cell.animationView.image = UIImage.animatedImage(with: blueAnimation, duration: 2)
-            cell.colorView.image = UIImage(named: "cardBackground04")
-        case 2:
+        case "cardBackground03":
             cell.animationView.image = UIImage.animatedImage(with: greenAnimation, duration: 2)
-            cell.colorView.image = UIImage(named: "cardBackground03")
-        case 3:
+        case "cardBackground02":
             cell.animationView.image = UIImage.animatedImage(with: grayAnimation, duration: 2)
-            cell.colorView.image = UIImage(named: "cardBackground02")
-        case 4:
-            cell.animationView.image = UIImage.animatedImage(with: grayAnimation, duration: 2)
-            cell.colorView.image = UIImage(named: "cardBackground01")
-            
-        case 5:
-            cell.cardView.image = UIImage(named: playRecords[0]["charactor"]!)
-            cell.colorView.image = UIImage(named: playRecords[0]["background"]!)
-            cell.animationView.image = UIImage.animatedImage(with: redAnimation, duration: 2)
         default:
             break
         }
