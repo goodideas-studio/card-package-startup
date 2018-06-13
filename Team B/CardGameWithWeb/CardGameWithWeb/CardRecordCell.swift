ef8b2569a9823b618ed696b2b555ebd23487a030
Morris Lu <morris.lwt@gmail.com>
2018-06-13 11:26:16 +0800

    add collectionview

---
+++ Team B/CardGameWithWeb/CardGameWithWeb/CardRecordCell.swift
@@ -0,0 +1,19 @@
+//
+//  CardRecordCell.swift
+//  CardGameWithWeb
+//
+//  Created by Morris on 2018/6/13.
+//  Copyright © 2018 Morris. All rights reserved.
+//
+
+import UIKit
+
+class CardRecordCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var animationView: UIImageView!
+    @IBOutlet weak var colorView: UIImageView!
+    @IBOutlet weak var cardView: UIImageView!
+    
+    
+}
--- Team B/CardGameWithWeb/CardGameWithWeb/RecordViewController.swift
+++ Team B/CardGameWithWeb/CardGameWithWeb/RecordViewController.swift
@@ -9,5 +9,34 @@
 import UIKit
 
 class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
     
+}
+extension RecordViewController:UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CardRecordCell
+        cell.backgroundColor = .gray
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //        let height = (view.frame.height - 20 ) / 3
+        //        let width = (view.frame.width - 20) / 3
+        
+        return CGSize(width: 123, height: 150)
+    }
 }
