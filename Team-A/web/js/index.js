5ee0bc87d3a62d71404eb228dc6fc76b1cb26e03
andyka1714 <andyka1714@gmail.com>
2018-06-13 11:52:28 +0800

    add meta viewport

--- Team-A/web/js/index.js
+++ Team-A/web/js/index.js
@@ -1,45 +1,45 @@
-window.onload = function() {
-  let rollList = [];
-  let cardCover = document.querySelector('.card-cover');
-  let cardEffect = document.querySelector('.card-effect');
+window.onload = function () {
+  let rollList = []
+  let cardCover = document.querySelector('.card-cover')
+  let cardEffect = document.querySelector('.card-effect')
 
-  for(let i = 0; i < 20; i++) {
-    let rand = Math.floor((Math.random() * cardList.length));
-    rollList.push(cardList[rand]);
+  for (let i = 0; i < 20; i++) {
+    let rand = Math.floor((Math.random() * cardList.length))
+    rollList.push(cardList[rand])
   }
 
-  let rollListDOM = document.querySelector('.roll-list');
-  for(let i = 0; i < rollList.length; i++) {
-    let newItem = document.createElement('li');
-    switch(rollList[i].type) {
-      case 0: newItem.classList.add('white-card'); break;
-      case 1: newItem.classList.add('green-card'); break;
-      case 2: newItem.classList.add('blue-card'); break;
-      case 3: newItem.classList.add('red-card'); break;
+  let rollListDOM = document.querySelector('.roll-list')
+  for (let i = 0; i < rollList.length; i++) {
+    let newItem = document.createElement('li')
+    switch (rollList[i].type) {
+      case 0: newItem.classList.add('white-card'); break
+      case 1: newItem.classList.add('green-card'); break
+      case 2: newItem.classList.add('blue-card'); break
+      case 3: newItem.classList.add('red-card'); break
     }
-    newImg = document.createElement('img');
-    newImg.src = `${rollList[i].src}`;
-    newItem.append(newImg);
-    rollListDOM.append(newItem);
+    newImg = document.createElement('img')
+    newImg.src = `${rollList[i].src}`
+    newItem.append(newImg)
+    rollListDOM.append(newItem)
   }
 
-  let rollBtn = document.querySelector('.roll-btn');
+  let rollBtn = document.querySelector('.roll-btn')
   rollBtn.addEventListener('click', e => {
-    removeGlow(cardEffect);
-    turnOver(cardCover);
-    roll(rollListDOM);
-  });
-};
-
-function removeGlow(element) {
-  element.classList.remove('glow');
+    removeGlow(cardEffect)
+    turnOver(cardCover)
+    roll(rollListDOM)
+  })
 }
 
-function turnOver(element) {
-  element.classList.add('disable');
+function removeGlow (element) {
+  element.classList.remove('glow')
 }
 
-function roll(element) {
+function turnOver (element) {
+  element.classList.add('disable')
+}
+
+function roll (element) {
   element.classList.add('rolled')
 }
 
@@ -84,4 +84,4 @@ let cardList = [
     src: 'imgs/9.png',
     type: 1
   }
-];
+]
